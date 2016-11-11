4310c4dfc5c45d4ad122efd85e4ba06449e60a36
Louis Minsky <lminsky@packer.edu>
2016-11-11 16:24:21 -0500

    added pong so far

---
+++ Classwork/2016-11-11_pong/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1000, 600);        //Create the canvas
+
+  background(0);                  //Make the background black
+
+  fill(255);                      //Change the fill color to white
+
+  ellipse(500, 300, 100, 100);    //Draw the circle in the center of the screen
+
+  rect(10, 250, 20, 100);         //Draw the rectangle on the left side of the screen
+}
+
+function draw() {
+  
+}
