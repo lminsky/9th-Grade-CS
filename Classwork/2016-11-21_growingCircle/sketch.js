9f31f574ca67a4e7be1d7eaea505193a07d3ffc5
Louis Minsky <lminsky@packer.edu>
2016-11-21 14:22:07 -0500

    recenter the ellipse

--- Classwork/2016-11-21_growingCircle/sketch.js
+++ Classwork/2016-11-21_growingCircle/sketch.js
@@ -7,6 +7,6 @@ function setup() {
 
 function draw() {               //The draw loop will run as long as the page is open in your browser
 	background(255);              //Start by clearing the screen (this hides any previously drawn shapes)
-	ellipse(400, 400, d, d);      //Draw an ellipse in the center of the canvas with a diameter "d"
+	ellipse(300, 300, d, d);      //Draw an ellipse in the center of the canvas with a diameter "d"
 	d = d - 1;                    //Decrease the value of "d" by 1
 }
