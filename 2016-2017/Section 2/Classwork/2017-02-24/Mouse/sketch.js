cf15807bc458ca0722eabdb74ad6f9dcebfb50aa
Louis Minsky <lminsky@packer.edu>
2017-02-28 08:14:35 -0500

    work from last class

---
+++ 2016-2017/Section 2/Classwork/2017-02-24/Mouse/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0, 255, 255);
+  rect(mouseX - 100, mouseY - 15, 200, 30);
+}
---
+++ 2016-2017/Section 2/Classwork/2017-02-28/Pong/sketch.js
@@ -0,0 +1,35 @@
+var x;
+var y;
+var diameter = 100;
+var xSpeed = 5;
+var ySpeed = 7;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = width/4;
+  y = height/2;
+}
+
+function draw() {
+  background(255, 0, 255);
+  ellipse(x, y, diameter, diameter);
+  x = x + xSpeed;
+  y = y + ySpeed;
+
+  // This code makes the ball bounce off the bottom
+  if(y > height - diameter/2) {
+    ySpeed = ySpeed * -1;
+  }
+
+  if(y <= diameter/2) {
+    ySpeed = ySpeed * -1;
+  }
+
+  if(x > width - diameter/2) {
+    xSpeed = xSpeed * -1;
+  }
+
+  if(x <= diameter/2) {
+    xSpeed = xSpeed * -1;
+  }
+}
